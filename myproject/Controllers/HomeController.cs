53e8dc1d74d6ac18d258a12e86779c38b61c7ee9
jemimahcad01@gmail.com <jemimahcad01@gmail.com>
2024-03-16 18:04:35 +0530

    files added

--- myproject/Controllers/HomeController.cs
+++ myproject/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Azure.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using myproject.Models;
@@ -16,19 +17,17 @@ public class HomeController : Controller
 
     SqlConnection con =new SqlConnection();
     SqlCommand com= new SqlCommand();
-    SqlDataReader dr;
+    SqlDataReader? dr;
 
     public IActionResult Index()
     {
         return View();
     }
-
     public IActionResult Privacy()
     {
         return View();
     }
-    
-     public IActionResult contactus()
+    public IActionResult contactus()
     {
         return View();
     }
@@ -44,11 +43,39 @@ public class HomeController : Controller
     {
         return View();
     }
-    void connectionString(){
-        con.connectionString"data source=192.168.1.240\SQLEXPRESS;database:cad_oos_shop;userid:CADBATCH01;password:CAD@123;TrustCertificate=True";
+    void ConnectionString()
+    {
+    con.ConnectionString="data source=192.168.1.240\\SQLEXPRESS;database:cad_oos_shop;userid:CADBATCH01;password:CAD@123;TrustCertificate=True;";
+    }
+
+    [HttpPost]
+    public IActionResult VerifyLogin(LoginModel lmodel){
+        ConnectionString();
+        con.Open();
+        com.Connection=con;
+        com.CommandText="select*from where loginuser username='"+ lmodel.username +"'+and password ='" + lmodel.password +"';";
+        dr=com.ExecuteReader();
+        if(dr.Read())
+        {
+            con.Close();
+            return View("success");
+        }
+        else
+        {
+            con.Close();
+            return View("error");
+        }
     }
 
       public IActionResult register()
+    {
+        return View();
+    }
+     public IActionResult error()
+    {
+        return View();
+    }
+     public IActionResult success()
     {
         return View();
     }
@@ -58,4 +85,5 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
 }
---
+++ myproject/Models/LoginModel.cs
@@ -0,0 +1,9 @@
+namespace myproject.Models;
+
+public class LoginModel
+{
+    public string? username { get; set; }
+    public string? password { get; set; }
+
+    
+}
